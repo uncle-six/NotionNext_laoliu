a43c1ff7d21f9aef573bef9e77222203c1861323
laoliu <dczhang01@gmail.com>
2026-04-02 22:34:51 +0800

    继续修改，以期一致

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -28,7 +28,7 @@ const CONFIG = {
   HEXO_POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
   HEXO_POST_LIST_SUMMARY: true, // 文章摘要
   HEXO_POST_LIST_PREVIEW: false, // 读取文章预览
-  HEXO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
+  HEXO_POST_LIST_IMG_CROSSOVER: , // 博客列表图片左右交错
 
   HEXO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
   HEXO_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
@@ -42,6 +42,6 @@ const CONFIG = {
   HEXO_WIDGET_DARK_MODE: true, // 夜间模式
   HEXO_WIDGET_TOC: true, // 移动端悬浮目录
 
-  HEXO_THEME_COLOR: '#928CEE' // 主题色配置（默认为 #928CEE）
+  HEXO_THEME_COLOR: '#000908' // 主题色配置（默认为 #928CEE）
 }
 export default CONFIG
